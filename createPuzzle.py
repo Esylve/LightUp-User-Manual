b9451fa528217c4c73c94d04d9331c2a76a81230
Erin Sylve <Esylve@users.noreply.github.com>
2020-04-15 16:40:20 -0500

    The whole cannoli
    Just built the whole thing

---
+++ createPuzzle.py
@@ -0,0 +1,18 @@
+#First get html page from site imported. 
+#Use BeautifulSoup to extract placement and assc ID
+#Store info into a matrix 
+    #The info needs to 
+
+   for i = 0 to 624
+   soup.find_all ('id="square'+(i)+''"')
+    if color.get('src') = "clue0" then 
+        
+    elif color.get('src') = "clue1" then
+
+    elif color.get('src') = "clue2" then
+
+    elif color.get('src') = "clue3" then
+
+    elif color.get('src') = "clue4" then
+
+    else 
